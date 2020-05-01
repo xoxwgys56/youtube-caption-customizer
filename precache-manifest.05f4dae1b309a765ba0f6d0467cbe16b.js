c202332a2c5c451a4d828fef66eb11cd6dc11a41
xoxwgys56 <lololtoday@gmail.com>
2020-05-02 00:04:37 +0900

    Updates

---
+++ precache-manifest.05f4dae1b309a765ba0f6d0467cbe16b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d9190fecadcdf8407810c240b9d10f",
+    "url": "/youtube-caption-customizer/index.html"
+  },
+  {
+    "revision": "ceb3a75fadaa02e21f3c",
+    "url": "/youtube-caption-customizer/static/css/2.5bbf332f.chunk.css"
+  },
+  {
+    "revision": "f6bd9cc955b49197312d",
+    "url": "/youtube-caption-customizer/static/css/main.2c32f38f.chunk.css"
+  },
+  {
+    "revision": "ceb3a75fadaa02e21f3c",
+    "url": "/youtube-caption-customizer/static/js/2.7ef02094.chunk.js"
+  },
+  {
+    "revision": "57757184ab1147fc6a610604e30dd6ea",
+    "url": "/youtube-caption-customizer/static/js/2.7ef02094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6bd9cc955b49197312d",
+    "url": "/youtube-caption-customizer/static/js/main.3f5e86a5.chunk.js"
+  },
+  {
+    "revision": "aaf4f6b1789ba51b0349",
+    "url": "/youtube-caption-customizer/static/js/runtime-main.db819917.js"
+  }
+]);
